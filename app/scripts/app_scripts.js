8dbcf7c237ddffcc16bcab617d33c51400eba0fd
unknown <kblaszczyk@ARTE176.media.non.3dart.com>
2015-07-28 13:19:36 +0200

    Guide steps seems to working correctly

--- app/scripts/app_scripts.js
+++ app/scripts/app_scripts.js
@@ -22,15 +22,47 @@ var initializer = {
         var guide_container = document.getElementById('guide'),
             language_containers = guide_container.querySelectorAll('div[data-language]'),
             language_change = document.getElementById('language-change'),
-            language_changer = language_change.querySelectorAll('span[data-language]');
+            language_changer = language_change.querySelectorAll('span[data-language]'),
+            skip_btns = guide_container.querySelectorAll('a[data-href="skip"]');
 
+        //count steps-update function
+        function language_steps() {
+            var active_container = guide_container.querySelector('div[data-language].active'),
+                all_steps = active_container.querySelectorAll('div[data-step]');
+
+            active_container.querySelector('div.progress-count').querySelector('span[data-guide-count').innerHTML = all_steps.length;
+            update_step_status();
+        }
+        function update_step_status() {
+            var active_container = guide_container.querySelector('div[data-language].active'),
+                all_steps = active_container.querySelectorAll('div[data-step]');
+                //
+            var count = 0;
+            for (var i=0; i < all_steps.length; i++) {
+                all_steps[i].classList.contains('hidden') ? count++ : 0;
+                active_container.querySelector('div.progress-count').querySelector('span[data-progress-count').innerHTML = count;
+                count === all_steps.length ? guide_container.classList.add('hidden') : null;
+            }
+        }
+        //count all steps
+        language_steps();
+        //skip buttons in any langueges 
+        for (var i=0; i < skip_btns.length; i++) {
+            skip_btns[i].addEventListener('click', function() {
+                guide_container.classList.add('hidden');
+            })
+        }
         //hide guide steps
-        //
         for (var i=0; i < language_containers.length; i++) {
 
-            for (var j=0; j<language_containers.length; j++) {
+            for (var j=0; j < language_containers.length; j++) {
+
                 language_containers[i].querySelectorAll('div[data-step]')[j].querySelector('i.fa-times').addEventListener('click', function() {
-                    this.parentNode.classList.add('hidden');
+                    var node = this.parentNode
+
+                    node.classList.add('hidden');
+                    update_step_status();
+                    // node.parentNode.querySelector('div.progress-count').querySelector('span[data-progress-count]').innerHTML = count_all_steps;
                     //add to this.parent.parent counter if (steps_c/all_steps_c) {guide_container.classList.add('hidden') }
                 });
             }
